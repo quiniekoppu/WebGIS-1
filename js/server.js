98eea482bdd7a7cd74c21fc729dc25cb907f4ff1
Quiin <ntnq24122003@gmail.com>
2026-03-30 14:47:12 +0700

    q

--- js/server.js
+++ js/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const db = require('./db');
+//const db = require('./db');
 
 const app = express();
 app.use(cors());
